9d382d77a8377a7f916b8fa694cdfedce67de381
mayyu9 <coolmahimsd@gmail.com>
2024-02-02 22:27:00 +0530

    get request created

---
+++ README.md
@@ -0,0 +1,6 @@
+in this project, we mimic the contacts application.
+
+get request '/' - read the contacts and diaply the contacts as json.
+
+post request - write the data to the contacts file.
+
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+
+
+const app = express();
+const port = '8000';
+const filePath = path.join('contacts.json');
+
+async function readFileData() {
+    try{
+        const fileContent = await fs.readFile(filePath, 'utf8');
+        return JSON.parse(fileContent);
+    }
+    catch(error){
+        return [];
+    }
+}
+
+app.get('/', async (req, res) => {
+    try {
+        const fileData = await readFileData();
+        // console.log('thakur: ', fileData, Array.isArray(fileData), fileData.length)
+        if(fileData.length === 0) {
+            res.json({message: 'No contacts found'})
+        }
+        res.json(fileData);
+    } catch(err){
+        res.status(404).json({message: 'Error Data Not Found'})
+    }
+    
+})
+
+app.listen(port, () => console.log('contacts app running on 8000'));
